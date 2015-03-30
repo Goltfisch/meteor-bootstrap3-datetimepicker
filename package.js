549d64673a1c5268e48e1f2f008700a751ad6572
Alexander Wolf <alexander@goltfisch.de>
2015-03-30 09:53:04 +0200

    Add version constraints to packages
    Add version constraints to packages because it is needed.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     name: "goltfisch:bootstrap3-datetimepicker",
     summary: "Bootstrap 3 DateTime picker from @Eonasdan, packaged for Meteor.js with rzymek:moment dependency",
-    version: "0.1.0",
+    version: "0.2.0",
     git: "https://github.com/Goltfisch/meteor-bootstrap3-datetimepicker"
 });
 
 Package.onUse(function (api) {
-    api.use('jquery', 'client');
-    api.use('rzymek:moment', 'client');
+    api.use('jquery@1.11.3_2', 'client');
+    api.use('rzymek:moment@2.8.3_10', 'client');
 
     api.addFiles([
       'lib/css/bootstrap3-datetimepicker.css', // Add Date Time Picker CSS and JS files
